e0e8bcd7471b8c5895006ae4564e14c0ec7b9f73
DanOsad <dan@osadtsuk.com>
2022-05-04 17:45:48 +0300

    adding another challenge for today

--- 2022-05-04/app.js
+++ 2022-05-04/app.js
@@ -8,7 +8,7 @@ function spinWords(str){
     return r.join(' ')
   }
 
-  testCases = [
+testCases = [
     [spinWords("Welcome"), "emocleW"],
     [spinWords("Hey fellow warriors"), "Hey wollef sroirraw"],
     [spinWords("This is a test"), "This is a test"],
@@ -16,9 +16,9 @@ function spinWords(str){
     [spinWords("You are almost to the last test"), "You are tsomla to the last test"],
     [spinWords("Just kidding there is still one more"), "Just gniddik ereht is llits one more"],
     [spinWords("Seriously this is the last one"), "ylsuoireS this is the last one"],
-  ]
+]
 
-  function testSpinWords(tests){
+function testSpinWords(tests){
     tests.forEach(test=>console.log(test[0]===test[1]))
 }
 
