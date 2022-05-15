75c75fa1405356f5f4f36c483a7638d26ecaa10c
DanOsad <dan@osadtsuk.com>
2022-05-15 13:52:17 +0300

    7kyu problem

---
+++ 5kyu/moveZerosToEnd.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/52597aa56021e91c93000cb0
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+function moveZeros(arr) {
+    let result = []
+    arr.forEach((n,i)=> n===0 ? result.push(i) : null)
+    let newArr = arr.filter(n=>n!==0)
+    result.forEach(n=>newArr.push(0))
+    return newArr
+}
