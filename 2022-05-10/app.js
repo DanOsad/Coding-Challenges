3defafda598273df0f09b6df1d38432627275989
Dan <dan@osadtsuk.com>
2022-05-21 14:27:44 +0300

    removed old dated folders

--- 2022-05-10/app.js
+++
@@ -1,9 +0,0 @@
-// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
-
-function moveZeros(arr) {
-    let result = []
-    arr.forEach((n,i)=> n===0 ? result.push(i) : null)
-    let newArr = arr.filter(n=>n!==0)
-    result.forEach(n=>newArr.push(0))
-    return newArr
-}
