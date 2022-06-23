4a5d44a8548ad50c4e1d361eacc454aa5c082b3d
DanOsad <dan@osadtsuk.com>
2022-06-23 17:59:20 +0300

    new lc easy challenge

--- LC Easy/removeDuplicatesFromSortedArray.js
+++ LC Easy/removeDuplicatesFromSortedArray.js
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
 const removeDuplicates = nums => {
+    let j = 1
     for (let i=1; i<nums.length; i++) {
-      if (nums[i]==nums[i-1]) {
-        nums.splice(i, 1)
+      if (nums[i] != nums[i-1]) {
+        nums[j] = nums[i]
+        j++
       }
-    return nums.length
     }
+    return j
   }
