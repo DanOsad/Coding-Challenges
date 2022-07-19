b3e21a5adaaa3c5b93426c780a0d08d96467b6f6
DanOsad <dan@osadtsuk.com>
2022-07-19 17:58:31 +0300

    6kyu challenge

---
+++ 6kyu/createPhoneNumber.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
+const phoneNumber = arr => {
+    return `(${arr[0]}${arr[1]}${arr[2]}) ${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}`
+  }
